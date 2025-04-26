8eadf231761c47a0c046e1600a4ea4b26de63ea8
MuturiJoseph <muturijoseph064@gmail.com>
2025-04-26 08:00:25 +0300

    Add Program and Client models with migrations, implement many-to-many relationship, phone encryption, and audit logging setup

---
+++ app/Models/Client.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+
+class Client extends Model
+{
+    use HasFactory, LogsActivity;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'date_of_birth',
+        'gender',
+        'phone'
+    ];
+
+    /**
+     * Configure audit logging.
+     */
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['first_name', 'last_name', 'email', 'phone'])
+            ->logOnlyDirty();
+    }
+
+    /**
+     * Encrypt phone number.
+     */
+    protected function phone(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value ? decrypt($value) : null,
+            set: fn($value) => $value ? encrypt($value) : null,
+        );
+    }
+
+    /**
+     * Get the programs this client is enrolled in.
+     */
+    public function programs()
+    {
+        return $this->belongsToMany(Program::class, 'client_program');
+    }
+}
---
+++ app/Models/Program.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
+
+class Program extends Model
+{
+    use HasFactory, LogsActivity;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = ['name', 'description'];
+
+    /**
+     * Configure audit logging.
+     */
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'description'])
+            ->logOnlyDirty();
+    }
+
+    /**
+     * Get the clients enrolled in this program.
+     */
+    public function clients()
+    {
+        return $this->belongsToMany(Client::class, 'client_program');
+    }
+}
